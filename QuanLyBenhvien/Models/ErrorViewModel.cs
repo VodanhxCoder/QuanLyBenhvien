e7aaa4c1ab90ebdf0e42fe1124beafd17c076ee1
VodanhxCoder <khue16102004@gmail.com>
2025-03-26 08:26:50 +0700

    Add project files.

---
+++ QuanLyBenhvien/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyBenhvien.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
